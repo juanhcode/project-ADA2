5f532279d4fd6be1e12a7d8037031e5dbebca24d
sebastian <sebastian.cifuentes0808@gmail.com>
2024-11-23 22:16:20 -0500

    Completado

---
+++ src/main/java/parte2/Voraz.java
@@ -0,0 +1,45 @@
+package parte2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Voraz {
+    public static void main(String[] args) {
+//        Tripleta t = new Tripleta(500, 100, 600);
+//        Tripleta t2 = new Tripleta(450, 400, 800);
+//        Tripleta t3 = new Tripleta(100, 0, 1000);
+        Tripleta t = new Tripleta(50, 20, 50);
+        Tripleta t2 = new Tripleta(39, 150, 200);
+        Tripleta t3 = new Tripleta(40, 120, 130);
+
+        List<Tripleta> tripletaList = List.of(t, t2, t3);
+
+        // Número total de acciones
+        int A = 200;
+
+        System.out.println(combinaciones(tripletaList, A));
+    }
+
+    public static int combinaciones(List<Tripleta> tripletaList, int A) {
+        List<Tripleta> mutableList = new ArrayList<>(tripletaList);
+
+        mutableList.sort(Comparator.comparingDouble(tripleta -> -tripleta.getP()));
+
+        int accionesRestantes = A;
+        int gananciaTotal = 0;
+
+        for (Tripleta tripleta : mutableList) {
+            int maxAcciones = Math.min(tripleta.getMa(), accionesRestantes);
+            System.out.println("maxAcciones: " + maxAcciones + " accionesRestantes: " + accionesRestantes + tripleta.getP());
+            gananciaTotal += maxAcciones * tripleta.getP();
+            accionesRestantes -= maxAcciones;
+
+            if (accionesRestantes == 0 || tripleta.getMi() > accionesRestantes) {
+                break;
+            }
+        }
+
+        return gananciaTotal;
+    }
+}
